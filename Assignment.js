a6435cb7336884071fda3c3dd833d0239919bacc
Malleboyena Bhagya Lakshmi <Malleboyena.Lakshmi@Ltts.com>
2021-03-02 09:52:28 +0530

    update

--- Assignment.js
+++ Assignment.js
@@ -7,14 +7,26 @@ function book(isbn,title,author,price,rating){
 }
 
 let manager=[]; 
-manager.push(addbook('isbn1', 'title1','author1','price1','rating1'));
-manager.push(addbook('isbn2', 'title2','author2','price2','rating2'));
-manager.push(addbook('isbn3', 'title3','author3','price3','rating3'));
-manager.push(addbook('isbn4', 'title4','author4','price4','rating4'));
-
-
-console.log('manager:',manager)
+manager.push(addbook('1', 'title1','author1','price1','rating1'));
+manager.push(addbook('1', 'title2','author2','price2','rating2'));
+manager.push(addbook('2', 'title3','author3','price3','rating3'));
+manager.push(addbook('2', 'title4','author4','price4','rating4'));
 
 function addbook(isbn,title,author,price,rating){
     return new book(isbn,title,author,price,rating);
 }
+console.log('manager:',manager);
+
+function filterByIsbn(isbnNum){
+    let isbnlist=[];
+        for(let manage in manager){
+             if(manager.isbn===isbnNum){
+                 isbnlist.push(manager);
+                 return isbnlist;
+                }
+                
+    }
+    console.log(isbnlist);
+}
+
+filterByIsbn(1);
